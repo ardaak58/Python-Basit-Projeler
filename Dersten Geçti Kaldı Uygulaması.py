d8b1e6b312bbefe6c41929d6e7440367976cf36e
Arda Işık Ak <152442154+ardaak58@users.noreply.github.com>
2025-03-23 15:47:36 +0300

    Add files via upload
    Pyhton da İf Else Yapısı Kullanılarak Basit Bir Not Uygulaması Yapıldı.

---
+++ Dersten Geçti Kaldı Uygulaması.py
@@ -0,0 +1,11 @@
+#Dışarıdan İki Sayı Alınacak ve Geçme Notu ve Alınan Notu Girilecek ve Öğrenci Geçti veya Kaldı Diyecek 
+# Notu 90 üzeri ise Aferin Lan desin
+gecmenotu=int(input('Geçme Notu Giriniz'))
+alinannot=int(input('Aldığın Notu Gir'))
+if alinannot>=gecmenotu:
+    print('Dersi Geçtiniz')
+    if alinannot>=90:
+        print('Çok Başarılısınız')
+else:
+    print('Kaldınız')
+print('Not İşlemi Tamamlandı')
