c43d7494e66ea88b110f3a35fa6bbb20680fd8e7
Arda Işık Ak <152442154+ardaak58@users.noreply.github.com>
2025-03-23 15:50:45 +0300

    Dersten Geçti Kaldı Uygulaması.py

--- Dersten Geçti Kaldı Uygulaması.py
+++ Dersten Geçti Kaldı Uygulaması.py
@@ -1,5 +1,5 @@
 #Dışarıdan İki Sayı Alınacak ve Geçme Notu ve Alınan Notu Girilecek ve Öğrenci Geçti veya Kaldı Diyecek 
-# Notu 90 üzeri ise Çok Başarılısınız Desin
+# Notu 90 üzeri ise Çok Başarılısınız Mesajı versin.
 gecmenotu=int(input('Geçme Notu Giriniz'))
 alinannot=int(input('Aldığın Notu Gir'))
 if alinannot>=gecmenotu:
